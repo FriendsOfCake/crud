c5da709a4f535e11b758deb352be389d7db14c04
AD7six <andydawson76@gmail.com>
2012-12-04 23:53:18 +0100

    add license block

--- Console/Command/TranslationsShell.php
+++ Console/Command/TranslationsShell.php
@@ -4,6 +4,13 @@ App::uses('TranslationsEvent', 'Crud.Controller/Event');
 
 /**
  * TranslationsShell
+ *
+ * Copyright 2010-2012, Nodes ApS. (http://www.nodesagency.com/)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Nodes ApS, 2012
  */
 class TranslationsShell extends AppShell {
 
