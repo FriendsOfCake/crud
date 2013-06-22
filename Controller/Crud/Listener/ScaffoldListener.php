a8ede36d5a00f7e6a983c5bf579935ae93076ee3
Christian Winther <cw@bownty.com>
2013-06-22 14:02:53 +0000

    doc blocks and no more using class properties

--- Controller/Crud/Listener/ScaffoldListener.php
+++ Controller/Crud/Listener/ScaffoldListener.php
@@ -3,28 +3,54 @@
 App::uses('CrudListener', 'Crud.Controller/Crud');
 App::uses('Inflector', 'Utility');
 
+/**
+ * Implements beforeRender event listener that uses the build-in
+ * scaffolding views in cakephp.
+ *
+ * Using this listener you don't have to bake your views when
+ * doing rapid prototyping of your application
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Christian Winther, 2013
+ */
 class ScaffoldListener extends CrudListener {
 
+/**
+ * List of events implemented by this class
+ *
+ * @return array
+ */
+	public function implementedEvents() {
+		return array('Crud.beforeRender' => 'beforeRender');
+	}
+
+/**
+ * Do all the magic needed for using the
+ * cakephp scaffold views
+ *
+ * @param CakeEvent
+ * @return void
+ */
 	public function beforeRender(CakeEvent $event) {
 		$subject = $event->subject;
 		$model = $subject->model;
 		$request = $subject->request;
 		$controller = $subject->controller;
 
-		$this->ScaffoldModel = $model;
-		$this->scaffoldTitle = Inflector::humanize(Inflector::underscore($controller->viewPath));
-		$this->scaffoldActions = $controller->scaffold;
-		$title = __d('cake', 'Scaffold :: ') . Inflector::humanize($request->action) . ' :: ' . $this->scaffoldTitle;
+		$scaffoldTitle = Inflector::humanize(Inflector::underscore($controller->viewPath));
+		$title = __d('cake', 'Scaffold :: ') . Inflector::humanize($request->action) . ' :: ' . $scaffoldTitle;
 
 		$modelClass = $controller->modelClass;
-		$primaryKey = $this->ScaffoldModel->primaryKey;
-		$displayField = $this->ScaffoldModel->displayField;
+		$primaryKey = $model->primaryKey;
+		$displayField = $model->displayField;
 		$singularVar = Inflector::variable($modelClass);
 		$pluralVar = Inflector::variable($controller->name);
 		$singularHumanName = Inflector::humanize(Inflector::underscore($modelClass));
 		$pluralHumanName = Inflector::humanize(Inflector::underscore($controller->name));
-		$scaffoldFields = array_keys($this->ScaffoldModel->schema());
-		$associations = $this->_associations();
+		$scaffoldFields = array_keys($model->schema());
+		$associations = $this->_associations($model);
 
 		$controller->set(compact(
 			'modelClass', 'primaryKey', 'displayField', 'singularVar', 'pluralVar',
@@ -40,16 +66,22 @@ class ScaffoldListener extends CrudListener {
 /**
  * Returns associations for controllers models.
  *
+ * @param Model $model
  * @return array Associations for model
  */
-	protected function _associations() {
-		$keys = array('belongsTo', 'hasOne', 'hasMany', 'hasAndBelongsToMany');
+	protected function _associations(Model $model) {
 		$associations = array();
 
-		foreach ($keys as $type) {
-			foreach ($this->ScaffoldModel->{$type} as $assocKey => $assocData) {
-				$associations[$type][$assocKey]['primaryKey'] =	$this->ScaffoldModel->{$assocKey}->primaryKey;
-				$associations[$type][$assocKey]['displayField'] =	$this->ScaffoldModel->{$assocKey}->displayField;
+		foreach ($model->associations() as $type) {
+			$associations[$type] = array();
+
+			if (empty($model->{$type})) {
+				continue;
+			}
+
+			foreach ($model->{$type} as $assocKey => $assocData) {
+				$associations[$type][$assocKey]['primaryKey'] =	$model->{$assocKey}->primaryKey;
+				$associations[$type][$assocKey]['displayField'] =	$model->{$assocKey}->displayField;
 				$associations[$type][$assocKey]['foreignKey'] =	$assocData['foreignKey'];
 
 				list($plugin, $model) = pluginSplit($assocData['className']);
