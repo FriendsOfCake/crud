99231d5a1e498c2c3ce52cb1368ccd27938c040c
Christian Winther <cw@bownty.com>
2013-07-09 13:38:55 +0000

    Add docblocks

--- Lib/Panel/CrudPanel.php
+++ Lib/Panel/CrudPanel.php
@@ -2,31 +2,42 @@
 
 App::uses ('DebugPanel', 'DebugKit.Lib');
 
+/**
+ * Crud debug panel in DebugKit
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Christian Winther, 2013
+ */
 class CrudPanel extends DebugPanel {
 
+/**
+ * Declare we are a plugin
+ *
+ * @var string
+ */
 	public $plugin = 'Crud';
 
-	public function startup(Controller $controller) {
-		if (!$controller->Crud->isActionMapped()) {
-			return;
-		}
-
-	}
-
+/**
+ * beforeRender callback
+ *
+ * @param Controller $controller
+ * @return void
+ */
 	public function beforeRender(Controller $controller) {
 		if (!$controller->Crud->isActionMapped()) {
 			return;
 		}
-
-		$Action = $controller->Crud->action();
-		$controller->set('CRUD_config', $controller->Crud->config());
-		$controller->set('CRUD_action_config', $Action->config());
 
 		$listener_config = array();
 		foreach ($controller->Crud->config('listeners') as $listener => $value) {
 			$listener_config[$listener] = $controller->Crud->listener($listener)->config();
 		}
 
+		$Action = $controller->Crud->action();
+		$controller->set('CRUD_config', $controller->Crud->config());
+		$controller->set('CRUD_action_config', $Action->config());
 		$controller->set('CRUD_listener_config', $listener_config);
 	}
 
