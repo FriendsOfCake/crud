26c17b9ff9b66c19fd3ea0be9db85a2ede07c4da
AD7six <andydawson76@gmail.com>
2013-07-10 18:38:30 +0000

    add a test for getControllers

--- Test/Case/Console/Command/TranslationsShellTest.php
+++ Test/Case/Console/Command/TranslationsShellTest.php
@@ -142,6 +142,7 @@ class TranslationsShellTest extends CakeTestCase {
 		);
 		$this->assertSame($expected, $this->Shell->lines);
 	}
+
 	public function testGenerateFile() {
 		$controller = new Controller(new CakeRequest());
 		$controller->Example = new StdClass(); // dummy
@@ -196,5 +197,104 @@ END;
 		$this->assertSame(trim($expected), trim($contents));
 
 		unlink($path);
+	}
+
+/**
+ * testGenerateFileFileExists
+ *
+ * Running the shell should only add missing translations,
+ * Without removing or corrupting existing translations.
+ *
+ * @return void
+ */
+	public function testGenerateFileFileExists() {
+		$expected = <<<END
+<?php
+
+/**
+ * Example CRUD Component translations
+ */
+__d('crud', 'Some other translation');
+__d('crud', 'Invalid id');
+__d('crud', 'Not found');
+__d('crud', 'Method not allowed. This action permits only {methods}');
+__d('crud', 'Successfully created example');
+__d('crud', 'Could not create example');
+__d('crud', 'Successfully updated example');
+__d('crud', 'Could not update example');
+__d('crud', 'Successfully deleted example');
+END;
+
+		$path = TMP . 'crud_translations_shell_test.php';
+		file_put_contents($path, $expected);
+
+		$controller = new Controller(new CakeRequest());
+		$controller->Example = new StdClass(); // dummy
+		$controller->modelClass = 'Example';
+		$controller->components = array(
+			'Crud.Crud' => array(
+				'actions' => array(
+					'index', 'add', 'edit', 'view', 'delete'
+				)
+			)
+		);
+		$controller->constructClasses();
+		$controller->startupProcess();
+
+		$this->Shell
+			->expects($this->once())
+			->method('_loadController')
+			->will($this->returnValue($controller));
+
+		$this->Shell
+			->expects($this->once())
+			->method('_getControllers')
+			->will($this->returnValue(array('Example')));
+
+		$this->Shell->path($path);
+		$this->Shell->generate();
+
+		$this->assertFileExists($path);
+
+		$onlyNewTranslation = "\n__d('crud', 'Could not delete example');";
+		$expected .= $onlyNewTranslation;
+		$contents = file_get_contents($path);
+
+		$this->assertSame(trim($expected), trim($contents), "Only expected one translation to be added");
+
+		unlink($path);
+	}
+
+/**
+ * testGetControllers
+ *
+ * Verify that it returns a list of controller names without the Controller suffix
+ *
+ * @return void
+ */
+	public function testGetControllers() {
+		$class = new ReflectionClass('TranslationsShell');
+		$method = $class->getMethod('_getControllers');
+		$method->setAccessible(true);
+
+		$this->Shell = $this->getMock(
+			'TranslationsShell',
+			array('in', 'out', 'hr', 'err', '_stop', '_loadController'),
+			array($this->out, $this->out, $this->in)
+		);
+
+		App::build(array(
+			'Controller' => array(CAKE . 'Test' . DS . 'test_app' . DS . 'Controller' . DS)
+		), App::RESET);
+
+		$expected = array(
+			'App',
+			'Pages',
+			'TestAppsError',
+			'TestsApps',
+			'TestsAppsPosts'
+		);
+		$controllers = $method->invoke($this->Shell, '_getControllers');
+		$this->assertSame($expected, $controllers);
 	}
 }
