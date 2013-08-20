bdbaaf4dd98db344cc3180e89d56d51109ffd38f
AD7six <andydawson76@gmail.com>
2013-08-20 13:37:07 +0000

    remove _cancel handling
    If the user cancels they should not be submitting post data at all.
    Where this logic was going to be used, we'll replace with a link

--- Test/Case/Controller/Crud/Action/EditCrudActionTest.php
+++ Test/Case/Controller/Crud/Action/EditCrudActionTest.php
@@ -274,11 +274,6 @@ class EditCrudActionTest extends CrudTestCase {
 			->method('_findRecord')
 			->with(1, 'count')
 			->will($this->returnValue(true));
-		$Request
-			->expects($this->at(0))
-			->method('data')
-			->with('_cancel')
-			->will($this->returnValue(false));
 		$Action
 			->expects($this->at($i++))
 			->method('_trigger')
@@ -302,7 +297,7 @@ class EditCrudActionTest extends CrudTestCase {
 			->with('afterSave', array('success' => true, 'created' => false, 'id' => 1))
 			->will($this->returnValue($CrudSubject));
 		$Request
-			->expects($this->at(1))
+			->expects($this->at(0))
 			->method('data')
 			->with('_add')
 			->will($this->returnValue(true));
@@ -376,11 +371,6 @@ class EditCrudActionTest extends CrudTestCase {
 			->method('_findRecord')
 			->with(1, 'count')
 			->will($this->returnValue(true));
-		$Request
-			->expects($this->at(0))
-			->method('data')
-			->with('_cancel')
-			->will($this->returnValue(false));
 		$Action
 			->expects($this->at($i++))
 			->method('_trigger')
@@ -404,12 +394,12 @@ class EditCrudActionTest extends CrudTestCase {
 			->with('afterSave', array('success' => true, 'created' => false, 'id' => 1))
 			->will($this->returnValue($CrudSubject));
 		$Request
-			->expects($this->at(1))
+			->expects($this->at(0))
 			->method('data')
 			->with('_add')
 			->will($this->returnValue(false));
 		$Request
-			->expects($this->at(2))
+			->expects($this->at(1))
 			->method('data')
 			->with('_edit')
 			->will($this->returnValue(true));
@@ -723,87 +713,6 @@ class EditCrudActionTest extends CrudTestCase {
 
 		$this->setReflectionClassInstance($Action);
 		$this->callProtectedMethod('_get', array(1), $Action);
-	}
-
-/**
- * Test that calling HTTP PUT on an edit action
- * with `_cancel` set in the POST data will cancel the form submission
- *
- * @return void
- */
-	public function testPutActionCancel() {
-		$data = array(
-			'_cancel' => '_cancel',
-			'Model' => array('id' => 1)
-		);
-
-		$CrudSubject = new CrudSubject();
-
-		$Request = $this->getMock('CakeRequest');
-		$Request->setMethods(array('data'));
-		$Request->data = $data;
-
-		$Controller = $this
-			->getMockBuilder('Controller')
-			->disableOriginalConstructor()
-			->setMethods(array('referer'))
-			->getMock();
-
-		$Model = $this->getMock('Model');
-
-		$i = 0;
-		$Action = $this
-			->getMockBuilder('EditCrudAction')
-			->disableOriginalConstructor()
-			->setMethods(array(
-				'_validateId', '_request', '_model', '_trigger',
-				'_controller', '_redirect', '_findRecord'
-			))
-			->getMock();
-		$Action
-			->expects($this->at($i++))
-			->method('_validateId')
-			->with(1)
-			->will($this->returnValue(true));
-		$Action
-			->expects($this->at($i++))
-			->method('_request')
-			->will($this->returnValue($Request));
-		$Action
-			->expects($this->at($i++))
-			->method('_model')
-			->will($this->returnValue($Model));
-		$Action
-			->expects($this->at($i++))
-			->method('_findRecord')
-			->with(1, 'count')
-			->will($this->returnValue(true));
-		$Request
-			->expects($this->at(0))
-			->method('data')
-			->with('_cancel')
-			->will($this->returnValue(true));
-		$Action
-			->expects($this->at($i++))
-			->method('_trigger')
-			->with('beforeCancel')
-			->will($this->returnValue($CrudSubject));
-		$Action
-			->expects($this->at($i++))
-			->method('_controller')
-			->will($this->returnValue($Controller));
-		$Controller
-			->expects($this->at(0))
-			->method('referer')
-			->with(array('action' => 'index'))
-			->will($this->returnValue(array('action' => 'index')));
-		$Action
-			->expects($this->at($i++))
-			->method('_redirect')
-			->with($CrudSubject, array('action' => 'index'));
-
-		$this->setReflectionClassInstance($Action);
-		$this->callProtectedMethod('_put', array(1), $Action);
 	}
 
 /**
