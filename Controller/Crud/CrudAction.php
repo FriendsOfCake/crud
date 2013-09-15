b3588e0918918826b95c6e19611911906223f333
Bryan Crowe <bryan@zapsolutions.com>
2013-09-15 05:07:45 -0400

    Condensed CrudAction::enable()

--- Controller/Crud/CrudAction.php
+++ Controller/Crud/CrudAction.php
@@ -100,8 +100,7 @@ abstract class CrudAction extends CrudBaseObject {
 		$Controller = $this->_controller();
 		$actionName = $this->config('action');
 
-		$pos = array_search($actionName, $Controller->methods);
-		if ($pos === false) {
+		if (!in_array($actionName, $Controller->methods)) {
 			$Controller->methods[] = $actionName;
 		}
 	}
