f4fa72182242c79f166c91e36ee7d52eceaea712
Frode Marton Meling <frode.meling@saltship.com>
2013-09-28 16:29:19 +0200

    Fixed phpcs issues.

--- Controller/Crud/Listener/ScaffoldListener.php
+++ Controller/Crud/Listener/ScaffoldListener.php
@@ -101,9 +101,9 @@ class ScaffoldListener extends CrudListener {
 		$singularHumanName = Inflector::humanize(Inflector::underscore($modelClass));
 		$pluralHumanName = Inflector::humanize(Inflector::underscore($controller->name));
 		$scaffoldFields = array_keys($model->schema());
-		if($request->isGet() && !$this->_action()->config('saveMethod')) {
-			$scaffoldFields = array_merge($scaffoldFields,array_keys($model->virtualFields));
-		} 
+		if ($request->isGet() && !$this->_action()->config('saveMethod')) {
+			$scaffoldFields = array_merge($scaffoldFields, array_keys($model->virtualFields));
+		}
 		$associations = $this->_associations($model);
 
 		$controller->set(compact(
