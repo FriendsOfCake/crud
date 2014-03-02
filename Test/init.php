20c35bb36a0ae358ecd1005bed5fef4c16cec706
Christian Winther <cw@bownty.com>
2014-03-02 12:27:19 +0000

    make travis run tests?

---
+++ Test/init.php
@@ -0,0 +1,8 @@
+<?php
+
+require dirname(__DIR__) . '/vendor/cakephp/cakephp/src/basics.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+Cake\Core\Configure::write('App', [
+	'namespace' => 'App'
+]);
