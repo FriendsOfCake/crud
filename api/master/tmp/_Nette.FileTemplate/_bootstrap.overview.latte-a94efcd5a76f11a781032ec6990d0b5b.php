54b8dea82a20f5c0a2f6b9919d7e2ebfd5297ed9
API autobot <jippignu@gmail.com>
2015-04-21 01:00:18 +0200

    [autobot] update API docs

--- api/master/tmp/_Nette.FileTemplate/_bootstrap.overview.latte-a94efcd5a76f11a781032ec6990d0b5b.php
+++ api/master/tmp/_Nette.FileTemplate/_bootstrap.overview.latte-a94efcd5a76f11a781032ec6990d0b5b.php
@@ -1,23 +1,23 @@
-<?php //netteCache[01]000399a:2:{s:4:"time";s:21:"0.59194700 1429569563";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:79:"/var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/overview.latte";i:2;i:1347136010;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:28:"$WCREV$ released on $WCDATE$";}}}?><?php
+<?php //netteCache[01]000399a:2:{s:4:"time";s:21:"0.22562100 1429570815";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:79:"/var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/overview.latte";i:2;i:1347136010;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:28:"$WCREV$ released on $WCDATE$";}}}?><?php
 
 // source file: /var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/overview.latte
 
 ?><?php
 // prolog Nette\Latte\Macros\CoreMacros
-list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, 'cuvgrcz8ap')
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, '5da9k4pe4i')
 ;
 // prolog Nette\Latte\Macros\UIMacros
 //
 // block title
 //
-if (!function_exists($_l->blocks['title'][] = '_lbe1ed763fe5_title')) { function _lbe1ed763fe5_title($_l, $_args) { extract($_args)
+if (!function_exists($_l->blocks['title'][] = '_lbda1152a76d_title')) { function _lbda1152a76d_title($_l, $_args) { extract($_args)
 ;echo Nette\Templating\Helpers::escapeHtml($config->title ?: 'Overview', ENT_NOQUOTES) ;
 }}
 
 //
 // block content
 //
-if (!function_exists($_l->blocks['content'][] = '_lb4b8f82b1f8_content')) { function _lb4b8f82b1f8_content($_l, $_args) { extract($_args)
+if (!function_exists($_l->blocks['content'][] = '_lb93ef209534_content')) { function _lb93ef209534_content($_l, $_args) { extract($_args)
 ?><div id="content">
 	<h1><?php call_user_func(reset($_l->blocks['title']), $_l, get_defined_vars()) ?></h1>
 
@@ -49,7 +49,7 @@ if (!function_exists($_l->blocks['content'][] = '_lb4b8f82b1f8_content')) { func
 	</table>
 <?php if ($iterations) ob_end_flush(); else ob_end_clean(); endif ?>
 
-<?php if (!$group): Nette\Latte\Macros\CoreMacros::includeTemplate('@elementlist.latte', $template->getParameters(), $_l->templates['cuvgrcz8ap'])->render() ;endif ?>
+<?php if (!$group): Nette\Latte\Macros\CoreMacros::includeTemplate('@elementlist.latte', $template->getParameters(), $_l->templates['5da9k4pe4i'])->render() ;endif ?>
 </div>
 <?php
 }}
--- api/master/tmp/_Nette.FileTemplate/_bootstrap.tree.latte-ec02eccc1204057dd17747f6bb0aeb66.php
+++ api/master/tmp/_Nette.FileTemplate/_bootstrap.tree.latte-ec02eccc1204057dd17747f6bb0aeb66.php
@@ -1,23 +1,23 @@
-<?php //netteCache[01]000395a:2:{s:4:"time";s:21:"0.29747700 1429569564";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:75:"/var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/tree.latte";i:2;i:1347136010;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:28:"$WCREV$ released on $WCDATE$";}}}?><?php
+<?php //netteCache[01]000395a:2:{s:4:"time";s:21:"0.02746300 1429570816";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:75:"/var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/tree.latte";i:2;i:1347136010;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:28:"$WCREV$ released on $WCDATE$";}}}?><?php
 
 // source file: /var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/tree.latte
 
 ?><?php
 // prolog Nette\Latte\Macros\CoreMacros
-list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, 'ec5xg1p2gh')
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, '01w8fk5p12')
 ;
 // prolog Nette\Latte\Macros\UIMacros
 //
 // block title
 //
-if (!function_exists($_l->blocks['title'][] = '_lbd774a830fa_title')) { function _lbd774a830fa_title($_l, $_args) { extract($_args)
+if (!function_exists($_l->blocks['title'][] = '_lbe09425cdba_title')) { function _lbe09425cdba_title($_l, $_args) { extract($_args)
 ?>Tree<?php
 }}
 
 //
 // block tree
 //
-if (!function_exists($_l->blocks['tree'][] = '_lb6cfa811a2c_tree')) { function _lb6cfa811a2c_tree($_l, $_args) { extract($_args)
+if (!function_exists($_l->blocks['tree'][] = '_lbf255ad625f_tree')) { function _lbf255ad625f_tree($_l, $_args) { extract($_args)
 ?><div class="tree">
 	<ul>
 <?php $level = -1 ;foreach ($tree as $reflectionName => $reflection): if ($level === $tree->getDepth()): ?>
@@ -73,7 +73,7 @@ if (!function_exists($_l->blocks['tree'][] = '_lb6cfa811a2c_tree')) { function _
 //
 // block content
 //
-if (!function_exists($_l->blocks['content'][] = '_lbdb054f3676_content')) { function _lbdb054f3676_content($_l, $_args) { extract($_args)
+if (!function_exists($_l->blocks['content'][] = '_lb7a4f1e25f7_content')) { function _lb7a4f1e25f7_content($_l, $_args) { extract($_args)
 ?><div id="content">
 	<h1><?php call_user_func(reset($_l->blocks['title']), $_l, get_defined_vars()) ?></h1>
 
