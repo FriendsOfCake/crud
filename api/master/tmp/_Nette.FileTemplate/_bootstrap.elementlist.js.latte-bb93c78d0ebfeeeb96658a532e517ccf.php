be509605a5f13f80b57d43c654d50ef17b13cf99
API autobot <jippignu@gmail.com>
2015-04-25 09:30:17 +0200

    [autobot] update API docs

--- api/master/tmp/_Nette.FileTemplate/_bootstrap.elementlist.js.latte-bb93c78d0ebfeeeb96658a532e517ccf.php
+++ api/master/tmp/_Nette.FileTemplate/_bootstrap.elementlist.js.latte-bb93c78d0ebfeeeb96658a532e517ccf.php
@@ -1,10 +1,10 @@
-<?php //netteCache[01]000405a:2:{s:4:"time";s:21:"0.03319100 1429945215";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:85:"/var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/elementlist.js.latte";i:2;i:1347136010;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:28:"$WCREV$ released on $WCDATE$";}}}?><?php
+<?php //netteCache[01]000405a:2:{s:4:"time";s:21:"0.21683900 1429947015";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:85:"/var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/elementlist.js.latte";i:2;i:1347136010;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:28:"$WCREV$ released on $WCDATE$";}}}?><?php
 
 // source file: /var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/elementlist.js.latte
 
 ?><?php
 // prolog Nette\Latte\Macros\CoreMacros
-list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, '6ezk3bnx3k')
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, 'a38d1p5p4f')
 ;
 // prolog Nette\Latte\Macros\UIMacros
 
