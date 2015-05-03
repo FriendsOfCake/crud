4c2995e54c8684a176e8a00763b8df1e063ebe86
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-05-03 00:45:11 -0400

    Initial Bulk/DeleteAction class

---
+++ src/Action/Bulk/DeleteAction.php
@@ -0,0 +1,49 @@
+<?php
+namespace Crud\Action\Bulk;
+
+use Cake\Controller\Controller;
+use Cake\ORM\Query;
+
+/**
+ * Handles Bulk 'Delete' Crud actions
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ */
+class DeleteAction extends BaseAction
+{
+    /**
+     * Constructor
+     *
+     * @param \Cake\Controller\Controller $Controller Controller instance
+     * @param array $config Default settings
+     * @return void
+     */
+    public function __construct(Controller $Controller, $config = [])
+    {
+        $this->_defaultConfig['actionName'] = 'Delete';
+        $this->_defaultConfig['messages'] = [
+            'success' => [
+                'text' => 'Delete completed successfully'
+            ],
+            'error' => [
+                'text' => 'Could not complete deletion'
+            ]
+        ];
+        return parent::__construct($controller, $config);
+    }
+
+    /**
+     * Handle a bulk delete
+     *
+     * @param \Cake\ORM\Query $query The query to act upon
+     * @return boolean
+     */
+    protected function _bulk(Query $query = null)
+    {
+        $query = $this->query()->delete();
+        $statement = $query->execute();
+        $statement->closeCursor();
+        return $statement->rowCount();
+    }
+}
