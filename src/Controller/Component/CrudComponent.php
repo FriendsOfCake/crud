2e3cd45111672190855f44798b56a41b957d920b
Jose Diaz-Gonzalez <myaccounts@savant.be>
2015-05-06 00:30:02 -0400

    Minor docblock update

--- src/Controller/Component/CrudComponent.php
+++ src/Controller/Component/CrudComponent.php
@@ -486,7 +486,7 @@ class CrudComponent extends Component
      * object.
      *
      * @param string $eventName Event name
-     * @param \Cake\Event\Subject $subject Event data
+     * @param \Cake\Event\Subject $data Event data
      * @throws \Exception if any event listener return a CakeResponse object.
      * @return \Cake\Event\Event
      */
