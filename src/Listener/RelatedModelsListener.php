b3188af3165ec3d335a561195e2bca5eeb0e4378
Stefan D <stefan@php-engineer.de>
2015-09-23 11:18:13 +0200

    related models conditions

--- src/Listener/RelatedModelsListener.php
+++ src/Listener/RelatedModelsListener.php
@@ -51,7 +51,7 @@ class RelatedModelsListener extends BaseListener
                 continue;
             }
 
-            $query = $association->find()->find('list');
+            $query = $association->find()->find('list', ['conditions' => $association->conditions()]);
             $subject = $this->_subject(compact('name', 'viewVar', 'query', 'association'));
             $event = $this->_trigger('relatedModel', $subject);
 
