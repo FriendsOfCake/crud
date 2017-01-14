67ef193b8d1917c4f2afa4a8beaadf96b279bc73
API autobot <jippignu@gmail.com>
2017-01-14 12:00:28 +0100

    [autobot] update API docs

--- api/master/tmp/_Nette.FileTemplate/_bootstrap.elementlist.js.latte-bb93c78d0ebfeeeb96658a532e517ccf.php
+++ api/master/tmp/_Nette.FileTemplate/_bootstrap.elementlist.js.latte-bb93c78d0ebfeeeb96658a532e517ccf.php
@@ -1,10 +1,10 @@
-<?php //netteCache[01]000405a:2:{s:4:"time";s:21:"0.89130400 1484305227";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:85:"/var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/elementlist.js.latte";i:2;i:1347136010;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:28:"$WCREV$ released on $WCDATE$";}}}?><?php
+<?php //netteCache[01]000405a:2:{s:4:"time";s:21:"0.51536600 1484391623";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:85:"/var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/elementlist.js.latte";i:2;i:1347136010;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:28:"$WCREV$ released on $WCDATE$";}}}?><?php
 
 // source file: /var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/elementlist.js.latte
 
 ?><?php
 // prolog Nette\Latte\Macros\CoreMacros
-list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, 'kush72e3h1')
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, '292z5jwseu')
 ;
 // prolog Nette\Latte\Macros\UIMacros
 
--- api/master/tmp/_Nette.FileTemplate/_bootstrap.tree.latte-ec02eccc1204057dd17747f6bb0aeb66.php
+++ api/master/tmp/_Nette.FileTemplate/_bootstrap.tree.latte-ec02eccc1204057dd17747f6bb0aeb66.php
@@ -1,23 +1,23 @@
-<?php //netteCache[01]000395a:2:{s:4:"time";s:21:"0.92049800 1484305230";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:75:"/var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/tree.latte";i:2;i:1347136010;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:28:"$WCREV$ released on $WCDATE$";}}}?><?php
+<?php //netteCache[01]000395a:2:{s:4:"time";s:21:"0.03243900 1484391626";s:9:"callbacks";a:2:{i:0;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:9:"checkFile";}i:1;s:75:"/var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/tree.latte";i:2;i:1347136010;}i:1;a:3:{i:0;a:2:{i:0;s:19:"Nette\Caching\Cache";i:1;s:10:"checkConst";}i:1;s:25:"Nette\Framework::REVISION";i:2;s:28:"$WCREV$ released on $WCDATE$";}}}?><?php
 
 // source file: /var/www/jippignu/data/www/cakephp.dk/apigen/templates/bootstrap/tree.latte
 
 ?><?php
 // prolog Nette\Latte\Macros\CoreMacros
-list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, '8x58kcxgmf')
+list($_l, $_g) = Nette\Latte\Macros\CoreMacros::initRuntime($template, '227eu96vuu')
 ;
 // prolog Nette\Latte\Macros\UIMacros
 //
 // block title
 //
-if (!function_exists($_l->blocks['title'][] = '_lbd336721c36_title')) { function _lbd336721c36_title($_l, $_args) { extract($_args)
+if (!function_exists($_l->blocks['title'][] = '_lbc528b57c8b_title')) { function _lbc528b57c8b_title($_l, $_args) { extract($_args)
 ?>Tree<?php
 }}
 
 //
 // block tree
 //
-if (!function_exists($_l->blocks['tree'][] = '_lb1e4ff1a5e0_tree')) { function _lb1e4ff1a5e0_tree($_l, $_args) { extract($_args)
+if (!function_exists($_l->blocks['tree'][] = '_lbe90be18c6c_tree')) { function _lbe90be18c6c_tree($_l, $_args) { extract($_args)
 ?><div class="tree">
 	<ul>
 <?php $level = -1 ;foreach ($tree as $reflectionName => $reflection): if ($level === $tree->getDepth()): ?>
@@ -73,7 +73,7 @@ if (!function_exists($_l->blocks['tree'][] = '_lb1e4ff1a5e0_tree')) { function _
 //
 // block content
 //
-if (!function_exists($_l->blocks['content'][] = '_lb29ad86ab12_content')) { function _lb29ad86ab12_content($_l, $_args) { extract($_args)
+if (!function_exists($_l->blocks['content'][] = '_lb5b0bf411c8_content')) { function _lb5b0bf411c8_content($_l, $_args) { extract($_args)
 ?><div id="content">
 	<h1><?php call_user_func(reset($_l->blocks['title']), $_l, get_defined_vars()) ?></h1>
 
