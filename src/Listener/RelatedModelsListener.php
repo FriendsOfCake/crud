5959afb8c15fec6bed6c97e5f6e80ad82b02625c
eymen_elkum <aymanalkom@gmail.com>
2017-03-27 00:16:12 +0300

    make relatedModels works with Crud.View

--- src/Listener/RelatedModelsListener.php
+++ src/Listener/RelatedModelsListener.php
@@ -25,6 +25,7 @@ class RelatedModelsListener extends BaseListener
     {
         return [
             'Crud.beforePaginate' => 'beforePaginate',
+            'Crud.beforeFind' => 'beforeFind',
             'Crud.beforeRender' => 'beforeRender',
         ];
     }
@@ -36,6 +37,28 @@ class RelatedModelsListener extends BaseListener
      * @return void
      */
     public function beforePaginate(Event $event)
+    {
+        $this->_containRelated($event);
+    }
+
+    /**
+     * Automatically parse and contain related table classes
+     *
+     * @param \Cake\Event\Event $event Before paginate event
+     * @return void
+     */
+    public function beforeFind(Event $event)
+    {
+        $this->_containRelated($event);
+    }
+
+    /**
+     * Automatically parse and contain related table classes
+     *
+     * @param \Cake\Event\Event $event Before paginate event
+     * @return void
+     */
+    protected function _containRelated(Event $event)
     {
         $contained = $event->subject()->query->contain();
         if (!empty($contained)) {
