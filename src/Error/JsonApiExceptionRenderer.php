f8514d653f3891f342e2d212628afabcf1a92796
Marcos Dantas <marcos.adantas@hotmail.com>
2017-04-11 16:35:04 -0300

    Exception by JsonApi added older Headers

--- src/Error/JsonApiExceptionRenderer.php
+++ src/Error/JsonApiExceptionRenderer.php
@@ -95,6 +95,7 @@ class JsonApiExceptionRenderer extends \Cake\Error\ExceptionRenderer
         }
 
         // set data and send response
+        $this->controller->response->header($this->error->responseHeader());
         $this->controller->response->type('jsonapi');
         $this->controller->response->body($json);
 
