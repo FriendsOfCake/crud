d861bb8098effc70a5594eb7494fc8e1c376faf0
inoas <mail@inoas.com>
2017-08-30 13:42:52 +0200

    rename Object_ to BaseObject

--- src/Core/Object.php
+++ src/Core/Object.php
@@ -1,6 +1,6 @@
 <?php
-// @deprecated Use class Object_ instead of Object PHP 7.2+; Will be removed in CRUD 6.x.
+// @deprecated Use class BaseObject instead of Object PHP 7.2+; Will be removed in CRUD 6.x.
 if (PHP_VERSION_ID < 72000) {
-    class_alias('Crud\Core\Object_', 'Crud\Core\Object');
+    class_alias('Crud\Core\BaseObject', 'Crud\Core\Object');
     trigger_error('Object is a reserved word in PHP 7.2+ and cannot be used as a class name.', E_USER_NOTICE);
 }
