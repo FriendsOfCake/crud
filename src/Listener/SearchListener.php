cee3dff6dac2e10330707fa7c58e4d485c8d8509
Marlin Cremers <m.cremers@cvo-technologies.com>
2017-12-30 13:48:18 +0000

    Support searching other datasource implementations
    Currently its not possible to use the search listener together
    with either the Muffin Webservice plugin, the CakePHP ElasticSearch
    plugin or any other datasource implementation.
    
    This change removes the hard dependencies on the behaviours used
    in with CakePHP's Table classes.
    
    Signed-off-by: Marlin Cremers <m.cremers@cvo-technologies.com>

--- src/Listener/SearchListener.php
+++ src/Listener/SearchListener.php
@@ -3,6 +3,7 @@ namespace Crud\Listener;
 
 use Cake\Core\Plugin;
 use Cake\Event\Event;
+use Cake\ORM\Table;
 use RuntimeException;
 
 class SearchListener extends BaseListener
@@ -47,16 +48,16 @@ class SearchListener extends BaseListener
             return;
         }
 
-        $table = $this->_table();
-        if (!$table->behaviors()->has('Search')) {
+        $repository = $this->_table();
+        if ($repository instanceof Table && !$repository->behaviors()->has('Search')) {
             throw new RuntimeException(sprintf(
                 'Missing Search.Search behavior on %s',
-                get_class($table)
+                get_class($repository)
             ));
         }
 
-        if ($table->behaviors()->hasMethod('filterParams')) {
-            $filterParams = $table->filterParams($this->_request()->query);
+        if ($repository instanceof Table && $repository->behaviors()->hasMethod('filterParams')) {
+            $filterParams = $repository->behaviors()->filterParams($this->_request()->query);
         } else {
             $filterParams = ['search' => $this->_request()->query];
         }
