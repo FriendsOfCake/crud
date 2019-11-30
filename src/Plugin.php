4f0710fdd98dda73b2d2736c77bfd4f6eefbbf04
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-11-29 22:59:20 -0500

    feat: create Plugin class

---
+++ src/Plugin.php
@@ -0,0 +1,16 @@
+<?php
+namespace Crud;
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin class for Crud
+ */
+class Plugin extends BasePlugin
+{
+    /**
+     * Plugin name.
+     *
+     * @var string
+     */
+    protected $name = 'Crud';
+}
