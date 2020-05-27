ef91eb59d236e58f8ae3893c4751bf9c6d50c0e7
Frank de Graaf (Phally) <frank@fdeg.nl>
2020-05-27 23:38:00 +0200

    Adds option to control which connections to log.
    Fixes #632

--- src/Listener/ApiQueryLogListener.php
+++ src/Listener/ApiQueryLogListener.php
@@ -22,6 +22,15 @@ class ApiQueryLogListener extends BaseListener
 {
 
     /**
+     * {@inheritDoc}
+     *
+     * `connections` List of connection names to log. Empty means all defined connections.
+     */
+    protected $_defaultConfig = [
+        'connections' => [],
+    ];
+
+    /**
      * Returns a list of all events that will fire in the controller during its lifecycle.
      * You can override this function to add you own listener callbacks
      *
@@ -49,7 +58,9 @@ class ApiQueryLogListener extends BaseListener
      */
     public function setupLogging(Event $event)
     {
-        foreach ($this->_getSources() as $connectionName) {
+        $connections = $this->getConfig('connections') ?: $this->_getSources();
+
+        foreach ($connections as $connectionName) {
             try {
                 $connection = $this->_getSource($connectionName);
 
