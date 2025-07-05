3ce0d87460b51904247c93e3abe7980df62c7f7e
ADmad <admad.coder@gmail.com>
2025-07-05 20:31:29 +0530

    Remove unused class

--- src/Core/Object.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// @deprecated Add backwards compat alias. "Object" is protected keyword in PHP 7.2
-if (PHP_VERSION_ID < 70200) {
-    class_alias('Crud\Core\BaseObject', 'Crud\Core\Object');
-}
